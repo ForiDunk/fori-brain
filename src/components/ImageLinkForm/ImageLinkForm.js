497761ff67219f3280ffe25fe36bb9b5c9afb81c
ForiDunk <szabolcs.forreiter@gmail.com>
2019-04-20 13:43:12 +0300

    css imageLinkForm

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -6,7 +6,7 @@ const ImageLinkForm = () => {
             <p className="f3">
                 {'This Magic Brain will detect faces in your pictures. Git it a try!'}
             </p>
-            <div>
+            <div className="center">
                 <input className="f4 pa2 w-70 center" type="text"/>
                 <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">Detect</button>
             </div>
@@ -14,4 +14,4 @@ const ImageLinkForm = () => {
     );
 };
 
-export default ImageLinkForm;
+export default ImageLinkForm; 
