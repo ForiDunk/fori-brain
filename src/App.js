4e0f1485c058650fc5fd6d2c52eedccc540fa024
bela-dev <fori-prg@pathwwway.com>
2019-04-22 11:51:26 +0300

    formatting

--- src/App.js
+++ src/App.js
@@ -78,37 +78,39 @@ class App extends Component {
       .catch(err => console.log(err));
   };
 
-  onRouteChange = (route) => {
+  onRouteChange = route => {
     if (route === 'signout') {
       this.setState({ isSignedIn: false });
     } else if (route === 'home') {
       this.setState({ isSignedIn: true });
     }
     this.setState({ route: route });
-  }
+  };
 
   render() {
     const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
         <Particles className="particles" params={particlesOptions} />
-        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
-        {route === 'home'
-          ? <div>
+        <Navigation
+          isSignedIn={isSignedIn}
+          onRouteChange={this.onRouteChange}
+        />
+        {route === 'home' ? (
+          <div>
             <Logo />
             <Rank />
             <ImageLinkForm
               onInputChange={this.onInputChange}
-              onButtonSubmit={this.onButtonSubmit} />
+              onButtonSubmit={this.onButtonSubmit}
+            />
             <FaceRecognition box={box} imageUrl={imageUrl} />
           </div>
-          : (
-            route === 'signin'
-              ? <Signin onRouteChange={this.onRouteChange} />
-              : <Register onRouteChange={this.onRouteChange} />
-          )
-
-        }
+        ) : route === 'signin' ? (
+          <Signin onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register onRouteChange={this.onRouteChange} />
+        )}
       </div>
     );
   }
