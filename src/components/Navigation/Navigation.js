d0e997af7e4a4a1cfc4a87e9b84cfb07e22cbfc6
ForiDunk <szabolcs.forreiter@gmail.com>
2019-04-22 20:14:27 +0300

    test

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './Navigation.css';
 
 const Navigation = () => {
-    return (
-        <nav className="navigation" style={{display: 'flex', justifyContent: 'flex-end'}}>
-            <p className="f3 link dim black underline pa3 pointer">Sign Out</p>
-        </nav>
-    );
+  return (
+    <nav
+      className="navigation"
+      style={{ display: 'flex', justifyContent: 'flex-end' }}>
+      <p className="f3 link dim black underline pa3 pointer">Sign Out</p>
+    </nav>
+  );
 };
 
 export default Navigation;
