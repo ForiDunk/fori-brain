23aa2f3557f101706995edb6f5199eb1bdd9e21d
bela-dev <fori-prg@pathwwway.com>
2019-04-25 11:11:39 +0300

    register hookup with backend

--- src/App.js
+++ src/App.js
@@ -42,8 +42,27 @@ class App extends Component {
       box: {},
       route: 'signin',
       isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: '',
+      },
     };
   }
+
+  loadUser = data => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+      },
+    });
+  };
 
   componentDidMount() {
     fetch('http://localhost:3000')
@@ -115,7 +134,10 @@ class App extends Component {
         ) : route === 'signin' ? (
           <Signin onRouteChange={this.onRouteChange} />
         ) : (
-          <Register onRouteChange={this.onRouteChange} />
+          <Register
+            loadUser={this.loadUser}
+            onRouteChange={this.onRouteChange}
+          />
         )}
       </div>
     );
